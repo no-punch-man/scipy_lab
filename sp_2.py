f42ffdc2666f7b0273268c2128c22a686e990d97
no-punch-man <92674993+no-punch-man@users.noreply.github.com>
2021-12-14 14:52:30 +0300

    Add files via upload

---
+++ sp_2.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as sc
+
+f = open(r'C:\Users\Anton\Downloads\large.txt', 'r')
+lines = f.readlines()
+l = []
+for line in lines:
+    l.append(line.rstrip())
+N = int(l[0])
+A = np.array([i.split() for i in l[1:N + 1]], dtype = np.float64)
+b = np.array(l[-1].split(), dtype = np.float64)
+x = np.linalg.solve(A, b)
+
+fig, ax = plt.subplots()
+fig.set_figheight(7)
+fig.set_figwidth(14)
+ax.grid(True)
+ax.bar(np.arange(x.size), x, width = 0.5)
+
+fig.savefig("large" + ".png")
